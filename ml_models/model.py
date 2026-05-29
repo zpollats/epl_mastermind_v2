e648be180a066e2a021b07f47f4205c68e1a39d8
Zach Pollatsek <zacharypollatsek@gmail.com>
2026-05-29 16:37:55 -0600

    update model script

--- ml_models/model.py
+++ ml_models/model.py
@@ -4,4 +4,8 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
+from sklearn.models import DummyClassifier, LogisticRegression, LinearRegression
+from sklearn.preprocessing import StandardScaler
+
+# Load data
 
